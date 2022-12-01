19c8c9bae47fb6c5097054ffde90d8a5404d48a7
tamara <tamarayustian@gmail.com>
2022-12-01 17:45:45 +0800

    build code editor

---
+++ src/components/CodeEditorWindow.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import Editor from "@monaco-editor/react";
+
+const CodeEditorWindow = ({ onChange, language, code, theme }) => {
+  const [value, setValue] = useState(code || "");
+
+  const handleEditorChange = (value) => {
+    setValue(value);
+    onChange("code", value);
+  };
+
+  return (
+    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      <Editor
+        heigh="85vh"
+        width="100%"
+        language={language || "javascript"}
+        value={value}
+        theme={theme}
+        defaultValue="// start code here"
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+};
+
+export default CodeEditorWindow;
---
+++ src/components/Landing.js
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+import CodeEditorWindow from "./CodeEditorWindow";
+import axios from "axios";
+
+const Landing = () => {};
+
+export default Landing;
